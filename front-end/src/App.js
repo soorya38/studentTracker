5bc2f160696e12626ae3316ae07f421eafbb0fa5
soorya38 <csasoorya@gmail.com>
2024-09-12 15:53:48 +0530

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,10 @@
+// import LoginPage from './LoginPage';
+import ParentsDashboard from './ParentsDashboard';
+
+const App = () => {
+  return (
+    <ParentsDashboard />
+  );
+}
+
+export default App;
---
+++ front-end/src/ParentsDashboard.js
@@ -0,0 +1,50 @@
+const ParentsDashboard = () => {
+    return (
+        <div class="container">
+            <Profile />
+            <TimeTable />
+            <Academics />
+            <SideBar />
+        </div>
+    );
+}
+
+const Profile = () => {
+    return (
+<div class="profile">
+                <div class="profile-pic"></div>
+                <p>Profile Details</p>
+            </div>
+    );
+}
+
+const TimeTable = () => {
+    return (
+        <div class="time-table">
+            <p>Time Table:</p>
+        </div>
+    );
+}
+
+const Academics = () => {
+    return (
+        <div class="academics">
+            <p>Academics</p>
+        </div>
+    );
+}
+
+const SideBar = () => {
+    return (
+        <div class="side-options">
+                <button>Teacher's Meet</button>
+                <button>Notifications</button>
+                <button>Rewards</button>
+                <button>Attendance</button>
+                <button>To-Do</button>
+                <button>Assignments</button>
+            </div>
+    );
+}
+
+export default ParentsDashboard;
